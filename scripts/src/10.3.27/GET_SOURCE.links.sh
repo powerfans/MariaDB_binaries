33fa6a21b5417717bd094946c2506c53138ba283
lsq <nikelsq@sina.com>
2021-01-21 11:43:44 +0800

    origin commit on 2021-1-20

---
+++ scripts/src/10.3.27/GET_SOURCE.links.sh
@@ -0,0 +1,8 @@
+##1.TAR source
+wget -c https://downloads.mariadb.com/MariaDB/mariadb-10.3.27/source/mariadb-10.3.27.tar.gz
+
+##2. SRC RPM
+wget -c https://mirrors.tuna.tsinghua.edu.cn/mariadb//mariadb-10.3.27/yum/rhel7-ppc64le/srpms/MariaDB-10.3.27-1.el7.centos.src.rpm
+
+
+
---
+++ scripts/src/10.5.8/GET_SOURCE.links.sh
@@ -0,0 +1,8 @@
+##1.TAR source
+wget -c https://downloads.mariadb.com/MariaDB/mariadb-10.5.8/source/mariadb-10.5.8.tar.gz
+
+##2. SRC RPM
+wget -c https://mirrors.tuna.tsinghua.edu.cn/mariadb//mariadb-10.5.8/yum/rhel7-ppc64le/srpms/MariaDB-10.5.8-1.el7.centos.src.rpm
+
+
+
